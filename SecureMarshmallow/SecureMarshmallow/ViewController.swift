12ead3c7bd459835a052a69927dbac3edd057ef0
junha <goodjunha@gmail.com>
2023-01-06 14:28:26 +0900

    📦:: project Setting

---
+++ SecureMarshmallow/SecureMarshmallow/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SecureMarshmallow
+//
+//  Created by 박준하 on 2023/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
