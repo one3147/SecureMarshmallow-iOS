ddcfc7f8cf0ef064b79f0262bb3b3e8153484c8e
junha <goodjunha@gmail.com>
2023-01-07 20:16:42 +0900

    🌫️:: error timer

--- SecureMarshmallow/SecureMarshmallow/ViewController.swift
+++ SecureMarshmallow/SecureMarshmallow/ViewController.swift
@@ -6,16 +6,46 @@
 //
 
 import UIKit
+import RxSwift
+import SnapKit
+import Then
 
 class ViewController: UIViewController {
-
+    
+    private let disposeBag = DisposeBag()
+    
+    private let errorTimer = UILabel().then {
+        $0.textAlignment = .center
+        $0.textColor = .systemBlue
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        view.backgroundColor = .red
+        self.view.backgroundColor = .ErrorColor
+        self.view.addSubview(errorTimer)
+        
+        self.errorTimer.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+        
+        self.setupPossibleBackgroundTimer()
     }
-
-
+    
+    private func setupPossibleBackgroundTimer() {
+        let startTime = Date()
+        
+        let timer = Observable<Int>.interval(
+            .seconds(1),
+            scheduler: MainScheduler.instance
+        )
+        timer.withUnretained(self)
+            .do(onNext: { weakSelf, countValue in
+                let elapseSeconds = Date().timeIntervalSince(startTime)
+                weakSelf.errorTimer.text = "타이머2: \(Int(elapseSeconds))초"
+            })
+                .subscribe()
+                .disposed(by: disposeBag)
+    }
 }
-
