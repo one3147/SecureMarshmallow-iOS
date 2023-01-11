68ce55c0b5167754ecb76bfc05aafefea2a9869e
junha <goodjunha@gmail.com>
2023-01-11 13:23:30 +0900

    👊:: Chapter 1

--- SecureMarshmallow/SecureMarshmallow/SceneDelegate.swift
+++ SecureMarshmallow/SecureMarshmallow/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             window = UIWindow(frame: UIScreen.main.bounds)
             window?.windowScene = windowScene
 
-        let rootNavigationController = UINavigationController(rootViewController: ErrorViewController())
+        let rootNavigationController = UINavigationController(rootViewController: StoryViewController())
         
         self.window?.rootViewController = rootNavigationController
         self.window?.makeKeyAndVisible()
---
+++ SecureMarshmallow/SecureMarshmallow/Story/StoryViewController.swift
@@ -0,0 +1,73 @@
+//
+//  StoryViewController.swift
+//  SecureMarshmallow
+//
+//  Created by 박준하 on 2023/01/11.
+//
+
+import UIKit
+import SnapKit
+import RxFlow
+import RxRelay
+import RxCocoa
+
+class StoryViewController: UIViewController {
+    
+    internal var logoImageView = UIImageView().then {
+        let imageName = "MarshmallowLogo"
+        $0.image = UIImage(named: imageName)
+    }
+    
+    internal var stratLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 24.0, weight: .black)
+        $0.text = "안녕하세요 마쉬멜로입니다.\n몇가지 유의사항 및 안내 드리겠습니다."
+        $0.numberOfLines = 2
+        $0.textAlignment = .center
+    }
+    
+    internal var okButton = UIButton().then {
+        $0.setTitle("넵! 알겠습니다!", for: .normal)
+        $0.setTitleColor(UIColor(ciColor: .black), for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 20.0, weight: .bold)
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 25.0
+    }
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        layout()
+        view.backgroundColor = .black
+        
+        okButton.rx.tap
+            .bind {
+                print("넵! 알겠습니다!")
+            }
+    }
+    
+    func layout() {
+        [
+            logoImageView,
+            stratLabel,
+            okButton
+        ].forEach { view.addSubview($0) }
+        
+        logoImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(130.0)
+            $0.centerX.equalToSuperview()
+            $0.height.width.equalTo(150.0)
+        }
+        
+        stratLabel.snp.makeConstraints {
+            $0.top.equalTo(logoImageView.snp.bottom).offset(155)
+            $0.centerX.equalToSuperview()
+        }
+        
+        okButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(stratLabel.snp.bottom).offset(255.0)
+            $0.height.equalTo(55.0)
+            $0.width.equalTo(260.0)
+        }
+    }
+}
