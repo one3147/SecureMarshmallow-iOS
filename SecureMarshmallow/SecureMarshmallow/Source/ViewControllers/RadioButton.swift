b8a7e11acd500c59c8ecea980171f1533e05e247
junha <goodjunha@gmail.com>
2023-02-06 20:42:03 +0900

    👊:: radio button 추가

---
+++ SecureMarshmallow/SecureMarshmallow/Source/ViewControllers/RadioButton.swift
@@ -0,0 +1,48 @@
+//
+//  RadioButton.swift
+//  Pods
+//
+//  Created by 박준하 on 2023/02/06.
+//
+
+import UIKit
+
+class RadioButton: UIButton {
+    var alternateButton:Array<RadioButton>?
+    
+    override func awakeFromNib() {
+        self.layer.borderWidth = 2.0
+        self.layer.masksToBounds = true
+    }
+    
+    func unselectAlternateButtons() {
+        if alternateButton != nil {
+            self.isSelected = true
+            
+            for aButton:RadioButton in alternateButton! {
+                aButton.isSelected = false
+            }
+        } else {
+            toggleButton()
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        unselectAlternateButtons()
+        super.touchesBegan(touches, with: event)
+    }
+    
+    func toggleButton() {
+        self.isSelected = !isSelected
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                self.layer.borderColor = UIColor.red.cgColor
+            } else {
+                self.layer.borderColor = UIColor.blue.cgColor
+            }
+        }
+    }
+}
